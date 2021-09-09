f6f98a87918672195c394dae4af5daed6681b2f4
Thomas Kolar <thomas.kolar@go.tarleton.edu>
2021-09-09 17:59:07 -0500

    Modified Main

--- DataStructures2/src/main/java/com/mycompany/datastructures2/Main.java
+++ DataStructures2/src/main/java/com/mycompany/datastructures2/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Main {
         public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Queue queue = new Queue(5);
+        Queue queue = new Queue(20);
         int m = sc.nextInt();
         while(m != 0)
         {
